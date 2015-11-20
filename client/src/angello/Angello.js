6891ee2c7aff923ff9adfcbba3d85267805104ab
yogi.jiang <yogi.jiang@hotmail.com>
2015-11-20 21:27:52 +0800

    15-11-20 21:27

--- client/src/angello/Angello.js
+++ client/src/angello/Angello.js
@@ -4,23 +4,47 @@
 var myModule = angular.module("Angello",
     [
         "ngRoute",
-        "ngAnimate",
-        "firebase",
-        "ngMessages",
-        "Angello.Common",
-        "Angello.Dashboard",
-        "Angello.Login",
-        "Angello.Storyboard",
-        "Angello.User",
-        "auth0",
-        "angular-jwt",
-        "angular-storage"
+        //"ngAnimate",
+        //"firebase",
+        //"ngMessages",
+        //"Angello.Common",
+        //"Angello.Dashboard",
+        //"Angello.Login",
+        //"Angello.Storyboard",
+        //"Angello.User",
+        //"auth0",
+        //"angular-jwt",
+        //"angular-storage"
     ]);
 
-myModule.config(function ($routeProvider) {
-    $routeProvider.when("/", {
-        templateUrl: "src/angello/storyboard/tmpl/storyboard.html",
-        controller: "StoryboardCtrl",
-        controllerAs: "storyboard"
-    });
-});
+//myModule.config(function ($routeProvider) {
+//    $routeProvider
+//        .when("/", {
+//        templateUrl: "src/angello/storyboard/tmpl/storyboard.html",
+//        controller: "StoryboardCtrl",
+//        controllerAs: "storyboard"
+//        })
+//        .when("/dashboard", {
+//            templateUrl: "src/angello/dashboard/tmpl/dashboard.html",
+//            controller: "LoginCtrl",
+//            controllerAs: "login"
+//        })
+//        .when("/users", {
+//            templateUrl: "src/angello/user/tmpl/users.html",
+//            controller: "UsersCtrl",
+//            controllerAs: "users"
+//        })
+//        .when("/users/:userId", {
+//            templateUrl: "src/angello/user/tmpl/user.html",
+//            controller: "UserCtrl",
+//            controllerAs: "myUser"
+//        })
+//        .when("/login", {
+//            templateUrl: "src/angello/login/tmpl/login.html",
+//            controller: "StoryboardCtrl",
+//            controllerAs: "storyboard"
+//        })
+//        .otherwise({ redirectTo: "/"});
+//});
+
+myModule.controller("MainCtrl", function () {});
